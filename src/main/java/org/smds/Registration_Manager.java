e3d5e69d4abc0dbd2514e3d5cb4a3d402408da6f
Sanjai-Amarnath <sanjaiamarnath08@gmail.com>
2025-09-20 09:44:49 +0530

    Added user controller

---
+++ src/main/java/org/smds/Registration_Manager.java
@@ -0,0 +1,12 @@
+package org.smds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Registration_Manager {
+    public static void main(String[] args) {
+        SpringApplication.run(Registration_Manager.class,args);
+    }
+}
---
+++ src/main/java/org/smds/controller/userController.java
@@ -0,0 +1,4 @@
+package org.smds.controller;
+
+public class userController {
+}
